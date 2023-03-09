ef924fc09ca6f7f5ed63d5b8e8f5a5ab373a46d6
VoliKoN <volvos.e@gmail.com>
2023-03-10 01:05:32 +0200

    Cleanup gh-pages branch, add html

--- dispatch/go.mod
+++
@@ -1,8 +0,0 @@
-module dispatch
-
-go 1.17
-
-require (
-	github.com/opentracing/opentracing-go v1.2.0
-	github.com/streadway/amqp v1.0.0
-)
--- ratings/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-envsubst '${PHP_HOST}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
-
-exec nginx-debug -g "daemon off;"
